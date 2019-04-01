daa39cf7c8c8c5b9f002cabcc6256a240fc3c3da
Imalli5253 <pnr.759@gmail.com>
2019-04-01 07:04:51 +0000

    Updates Source Code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		MAlli Container
+          This is my First Travis CI enabled React App DEployed at Elastic Ben stalk.
           </p>
           <a
             className="App-link"
